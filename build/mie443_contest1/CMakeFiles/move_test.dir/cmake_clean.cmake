60791250abc78892a639fd61214db91699365ec9
RisbudA <ricky_abhi6268@hotmail.com>
2019-01-15 18:18:42 -0500

    initial commit for mie443

---
+++ build/mie443_contest1/CMakeFiles/move_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/move_test.dir/src/test.cpp.o"
+  "/home/turtlebot/catkin_ws/devel/lib/mie443_contest1/move_test.pdb"
+  "/home/turtlebot/catkin_ws/devel/lib/mie443_contest1/move_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/move_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
